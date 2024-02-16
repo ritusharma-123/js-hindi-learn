f4cfacadc62001046fc918830dd0c92a69b43c67
ritusharma-123 <ritusharma1742003@gmail.com>
2024-02-16 23:19:59 +0530

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3)  // we are using nodejs ,not browser
+
+console.log(3+3);
+ console.log("ritu")
+
+let name = "ritu";
+let age = 19;
+let isLoggedIn = false;
+let state;
+
+// mumber => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object 
+
+console.log(typeof "ritu");
+console.log(typeof null);   // object
+console.log(typeof undefined);  // undefined
+
