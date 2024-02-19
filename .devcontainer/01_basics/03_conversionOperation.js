7831428d732e162a02c1d139ab124aa8cb124248
ritusharma-123 <ritusharma1742003@gmail.com>
2024-02-19 18:22:37 +0530

    conversion of operation

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -1,32 +1,73 @@
 // let score = "33abc"
 // let score = null;
 // let score = undefined;
-let score = true;
+// let score = true;
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
 // true = 1; false => 0
 
 // let isLoggedIn = 1
-let isLoggedIn = "";
+// let isLoggedIn = "";
 
 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1=> true ; 0=> false
 // "" => false
 // "ritu" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ***************** Operations **************************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2**3);
+
+
+// let str1 = "hello";
+// let str2 = " ritu";
+
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" +"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);  // 1
+// console.log(+"");  // 0
+
+
+// let num1,num2,num3
+
+// num1 = num2 = num3 = 2+2;
+
+// let gameCounter = 100;
+// ++gameCounter;
+// console.log(gameCounter);
+
+//link to study
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
