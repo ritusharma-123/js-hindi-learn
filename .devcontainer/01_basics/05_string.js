7bde159006bf8fa87434d167f6aefeaaebeb7c4a
ritusharma-123 <ritusharma1742003@gmail.com>
2024-02-20 11:23:07 +0530

    nums and math

---
+++ .devcontainer/01_basics/05_string.js
@@ -0,0 +1,70 @@
+const name = "ritu"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);  // string interpolation
+
+const gameName = new String('ritu-sh-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.split('-'));
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0,4);
+// console.log(newString);
+
+
+// const str1 = 'Mozilla';
+
+// console.log(str1.substring(2));
+
+
+const stringObj = new String('foo');
+
+console.log(stringObj.toString());
+
+
+// const anotherString = gameName.slice(-6,4)
+// console.log(anotherString);
+
+// const str = 'The quick brown fox jumps over the lazy dog.';
+
+// console.log(str.slice(4,19));
+// console.log(str.slice(-9,-5));
+
+// const newStringOne = "     ritu    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+
+// const paragraph = "I think Ruth's dog is cuter than your dog!";
+// console.log(paragraph.replace("think",'monkey'));
+
+
+
+
+// const url = "https://ritu.com/ritu%20sharma"
+
+// console.log(url.replace('%20','-'))
+
+// console.log(url.includes('ritu'))
+
+// console.log(url.includes('hello'))
+
+// console.log(gameName.split('-'));
+
+
+// const paragraph = 'The quick brown fox jumps over the lazy dog. It barked.';
+// const regex = /[A-Z]/g;
+
+// const found = paragraph.match(regex);
+// console.log(found);
+
+
--- .devcontainer/01_basics/datatypes-Summary.js
+++ .devcontainer/01_basics/datatypes-Summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123');
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 34566759838389494n
 
@@ -22,17 +22,42 @@ console.log(id === anotherId);
 
 // Array, Objects, Functions
 
-const heros = ["shaktiman","naagraj", "doga"];
-let myObj = {
-    name: "ritu",
-    age: 20,
-}
+// const heros = ["shaktiman","naagraj", "doga"];
+// let myObj = {
+//     name: "ritu",
+//     age: 20,
+// }
 
-const myFunction = function(){
-    console.log("Hello World");
-}
+// const myFunction = function(){
+//     console.log("Hello World");
+// }
 
-console.log(typeof bigNumber);
+// console.log(typeof bigNumber);
 
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-primitive)
+
+let myYoutubename = "ritusharmadotcom"
+
+let anothername = myYoutubename
+anothername = "Chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne 
+
+userTwo.email = "ritu@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
